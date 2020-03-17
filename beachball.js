6b7b28ebf11c49b982708305d4c7c5161103cdbb
Patricia Bettio <pfbettio@gmail.com>
2020-03-17 20:01:06 +0100

    exercise1

--- beachball.js
+++ beachball.js
@@ -1,1 +1,8 @@
 "use strict";
+import { gsap } from "gsap";
+
+//gsap.to(".ball", {duration: 3, rotation: 180, scale: 0.5});
+
+gsap.to(".ball", {duration: 2, x: 400});
+gsap.to(".ball", {duration: 2, rotation: 200});
+gsap.to(".ball", {delay: 2, scale: 0.5, y: 20});
