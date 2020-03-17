e3ef3dec8835e9c200285471698a3596714be21d
Patricia Bettio <pfbettio@gmail.com>
2020-03-17 21:02:26 +0100

    exercise2

--- multiballs.js
+++ multiballs.js
@@ -1,1 +1,7 @@
 "use strict";
+
+import { gsap } from "gsap";
+
+//gsap.to(".ball", {duration: 2, y: -400, stagger: 0.2, ease:"bounce"});
+gsap.to(".ball", {duration: 2, y: -400, stagger: 0.2,ease: "bounce.in"}, 0,2);
+//gsap.to(".ball", {y:-10})
